3db7ddac9910bd763c4bcbbbfe63f223d5f6ebc7
Javul Mulla <javulmulla8@gmail.com>
2022-08-11 07:59:46 +0530

    Javul: program modified

--- src/com/bridgelabz/linecomparison.java
+++ src/com/bridgelabz/linecomparison.java
@@ -8,6 +8,13 @@ public class LineComparison {
 	public static void LineComparision() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of a1: ");
+		int x1 = sc.nextInt();
+		System.out.println("Enter the value of x2: ");
+		int x2 = sc.nextInt();
+		System.out.println("Enter the value of y1: ");
+		int y1 = sc.nextInt();
+		System.out.println("Enter the value of y2: ");
+		int y2 = sc.nextInt();
 		int a1 = sc.nextInt();
 		System.out.println("Enter the value of a2: ");
 		int a2 = sc.nextInt();
@@ -16,13 +23,6 @@ public class LineComparison {
 		System.out.println("Enter the value of b2: ");
 		int b2 = sc.nextInt();
 		System.out.println("Enter the value of x1: ");
-		int x1 = sc.nextInt();
-		System.out.println("Enter the value of x2: ");
-		int x2 = sc.nextInt();
-		System.out.println("Enter the value of y1: ");
-		int y1 = sc.nextInt();
-		System.out.println("Enter the value of y2: ");
-		int y2 = sc.nextInt();
 		length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		System.out.println("Length of the line1 is: " + length1);
 		length2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
